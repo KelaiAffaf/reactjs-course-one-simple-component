a9977aadd860b93cc0496337054bff58bf652b42
Afaf KELAI <ea_kelai@esi.dz>
2021-12-16 21:46:34 +0100

    simple component with react js

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+
+function Header() {
+
+    return (
+        <div className='header-container'>
+            <h1>Our Company</h1>
+
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div className='main'>
+            <h3>Welcome to our Developement Company</h3>
+        </div>
+    )
+}
+
+export default Main
